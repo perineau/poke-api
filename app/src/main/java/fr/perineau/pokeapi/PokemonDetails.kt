f3f80daf208b56c2b15e8a4a47933a217b58c3db
LePygargue <46059303+LePygargue@users.noreply.github.com>
2021-02-17 11:09:25 +0100

    Pokemon Details XML
    Visuels de la page Pokemon Details

--- app/src/main/java/fr/perineau/pokeapi/PokemonDetails.kt
+++ app/src/main/java/fr/perineau/pokeapi/PokemonDetails.kt
@@ -5,14 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.fragment.navArgs
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 
 class PokemonDetails : Fragment() {
+
+
+
+    https://pokeapi.co/api/v2/pokemon/{arg.pokemon.id}
 
     val args: PokemonDetailsArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        args.pokemonId
+    }
+
+    private fun jsonParse() {
+        val url = "https://pokeapi.co/api/v2/pokemon/{pokemonId}"
+        val request = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+                response ->try {
+        })
+
     }
 
     override fun onCreateView(
